a5cc2e1cbe4dea7f0d6b87a1e55893a2b7fe4dfa
yj6326076 <yj6326076@hotmail.com>
2023-08-18 19:28:19 +0800

    init

---
+++ src/test/java/com/little/tmsserver/TmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.little.tmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
